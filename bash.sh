66f0f338d769a6fcc5778dae285a47793620b21b
KatoBeke <74418649+KatoBeke@users.noreply.github.com>
2021-04-13 15:17:32 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,4 @@
+#Dit is een file van de commandos die je nodig hebt om MongoDB te installeren
 sudo apt-get install gnupg
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
