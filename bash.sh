879e3da87fd144c9084c33944c56cf2a666449d2
KatoBeke <74418649+KatoBeke@users.noreply.github.com>
2021-04-13 17:37:22 +0200

    Delete bash.sh

--- bash.sh
+++
@@ -1,7 +0,0 @@
-#Dit zijn de commando's die je nodig hebt om MongoDB te installeren
-sudo apt-get install gnupg
-wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-sudo systemctl daemon-reload
