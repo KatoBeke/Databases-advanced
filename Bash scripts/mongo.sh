584f3b437d4628d0f4d343a0aa5b5967b6d302d3
KatoBeke <74418649+KatoBeke@users.noreply.github.com>
2021-04-13 21:28:37 +0200

    Add files via upload

--- Bash scripts/mongo.sh
+++ Bash scripts/mongo.sh
@@ -1,3 +1,5 @@
 #Dit zijn de commando's om MongoDB te starten
 sudo systemctl start mongod
 sudo systemctl status mongod
+sudo systemctl enable mongod
+mongo --eval 'db.runCommand({ connectionStatus: 1 })'
--- Bash scripts/redis.sh
+++ Bash scripts/redis.sh
@@ -1,21 +1,10 @@
 #Dit zijn de commando's die je nodig hebt om redis te installeren
-wget http :// download . redis . io / redis - stable . tar . gz
-tar xvzf redis - stable . tar . gz
-cd redis - stable
-make
-make test
-
 sudo apt update
 sudo apt install redis-server
 sudo systemctl status redis-server
 
 #Dit zijn de commando's om redis te starten
 sudo systemctl start redis
-cd src
-redis - server
-redis - cli ping
-redis - cli
-redis 127.0.0.1:6379 > ping
 
 #Dit is de commando om redis te stoppen 
 sudo systemctl stop redis
