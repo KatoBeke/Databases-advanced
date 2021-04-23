75e976b241f18a35c7a361b45b62a763bb67a323
KatoBeke <74418649+KatoBeke@users.noreply.github.com>
2021-04-23 19:23:18 +0200

    Update redis.sh

--- Bash scripts/redis.sh
+++ Bash scripts/redis.sh
@@ -2,8 +2,7 @@
 sudo apt-get install redis-server
 sudo systemctl status redis-server
 
-
-#Dit zijn de commando's om redis te starten
+#Dit is de commando om  om redis te starten
 sudo systemctl start redis
 
 #Dit is de commando om redis te stoppen 
