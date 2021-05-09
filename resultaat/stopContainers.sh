a0563354158f5e451981072543a7c96c0a280921
KatoBeke <74418649+KatoBeke@users.noreply.github.com>
2021-05-09 15:43:14 +0200

    Update stopContainers.sh

--- resultaat/stopContainers.sh
+++ resultaat/stopContainers.sh
@@ -1,6 +1,10 @@
-sudo systemctl stop redis
 sudo systemctl stop mongod
+sudo systemctl stop redis
 docker stop redis
 docker stop scraper
 docker stop mongo2
 docker stop parser
+docker rm redis
+docker rm scraper
+docker rm mongo2
+docker rm parser
