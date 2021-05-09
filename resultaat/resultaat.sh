1c9546e75066dbe70576170dd745b5f79095ed37
KatoBeke <74418649+KatoBeke@users.noreply.github.com>
2021-05-09 16:12:02 +0200

    Update resultaat.sh

--- resultaat/resultaat.sh
+++ resultaat/resultaat.sh
@@ -3,6 +3,16 @@ docker pull katobeke/databases-advanced-scraper:latest
 docker pull redis:latest
 docker pull katobeke/databases-advanced-parser:latest
 docker pull mongo:latest
+sudo systemctl stop mongod
+sudo systemctl stop redis
+docker stop redis
+docker stop scraper
+docker stop mongo2
+docker stop parser
+docker rm redis
+docker rm scraper
+docker rm mongo2
+docker rm parser
 docker run -d --name redis redis
 docker run -d --name scraper katobeke/databases-advanced-scraper:latest
 docker run -p 27017:27017 -d --name mongo2 mongo
